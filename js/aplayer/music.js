5fdcecdec8d54007d44ec61c302b0ee4259bea7b
Risingrode <3185087246@qq.com>
2023-09-03 22:32:06 +0800

    Site updated: 2023-09-03 22:32:01

---
+++ js/aplayer/music.js
@@ -0,0 +1,51 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    lrcType: 3,
+    showlrc: false,
+    autoHide: true,
+    audio: [
+        {
+            name: '纪念',
+            artist: '雷雨心',
+            url: 'http://music.163.com/song/media/outer/url?id=36229053.mp3',
+            cover: 'http://p1.music.126.net/LWmhB3XX8szN3V9TU-A5UA==/3390893862255408.jpg?param=130y130',
+            lrc: ''
+        },
+        {
+            name: '红尘轻剑',
+            artist: '魔道祖师',
+            url: 'http://music.163.com/song/media/outer/url?id=487220184.mp3',
+            cover: 'http://p2.music.126.net/twcVdaj_5UMOClUKY9Z8xA==/109951162959523806.jpg?param=130y130',
+            lrc: ''
+        },
+        {
+            name: '我用什么把你留住',
+            artist: '福寿禄乐队',
+            url: 'http://music.163.com/song/media/outer/url?id=1306923998.mp3',
+            cover: 'http://p2.music.126.net/RMYmJlVF2Fuvg2hmJNlmHA==/109951167343222893.jpg?param=130y130',
+            lrc: ''
+        },
+        {
+            name: '安娜的橱窗',
+            artist: '封茗囧菌',
+            url: 'http://music.163.com/song/media/outer/url?id=537470060.mp3',
+            cover: 'http://p2.music.126.net/wGiTjx3Mzhcds6UDMwPhQw==/109951167148145064.jpg?param=130y130',
+            lrc: ''
+        },
+        {
+            name: '致女皇',
+            artist: '抹茶茶子cha',
+            url: 'http://music.163.com/song/media/outer/url?id=2002976600.mp3',
+            cover: 'http://p2.music.126.net/f4iRaFpUfrsVl_QIrN5Djg==/109951168105047518.jpg?param=130y130',
+            lrc: ''
+        },
+        {
+            name: '凄美地',
+            artist: '郭顶',
+            url: 'http://music.163.com/song/media/outer/url?id=436346833.mp3',
+            cover: 'http://p2.music.126.net/wSMfGvFzOAYRU_yVIfquAA==/2946691248081599.jpg?param=130y130',
+            lrc: ''
+        },
+    ]
+});
